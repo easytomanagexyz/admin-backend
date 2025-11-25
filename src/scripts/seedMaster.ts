05bf05cde4dd6132ed3872c95076631834749798
easytomanagexyz <easytomanagexyz@gmail.com>
2025-11-25 13:16:27 +0530

    Update seedMaster.ts

--- src/scripts/seedMaster.ts
+++ src/scripts/seedMaster.ts
@@ -1,5 +1,5 @@
 // scripts/seedMaster.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/master";
 import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
